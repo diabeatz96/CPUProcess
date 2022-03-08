f348bc905c4b68a2cf5d50644dd0a3e52622d49b
bigbl <adamkostandy@gmail.com>
2022-03-08 13:00:43 -0500

    Commit the queue

---
+++ CPU.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bigbl on 3/8/2022.
+//
+
+#include "CPU.h"
---
+++ CPU.h
@@ -0,0 +1,14 @@
+//
+// Created by bigbl on 3/8/2022.
+//
+
+#ifndef CPUPROCESS_CPU_H
+#define CPUPROCESS_CPU_H
+
+
+class CPU {
+
+};
+
+
+#endif //CPUPROCESS_CPU_H
